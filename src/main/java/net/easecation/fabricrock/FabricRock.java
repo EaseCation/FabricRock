6f9b36287c3f194145b728a2f95c667df5d3068e
Choi-Jungwoo <jungwoo@cineuse.com>
2024-01-30 22:38:40 +0800

    initial commit

---
+++ src/main/java/net/easecation/fabricrock/FabricRock.java
@@ -0,0 +1,10 @@
+package net.easecation.fabricrock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricRock implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/easecation/fabricrock/client/FabricRockClient.java
@@ -0,0 +1,10 @@
+package net.easecation.fabricrock.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricRockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
