37587ffd3626dddb0ed68a4e6b855bc7794d1aa4
yuxuanchiadm <yuxuanchiadm@126.com>
2025-03-11 18:10:03 +0800

    Fix resource pack provider registering

---
+++ src/main/java/net/easecation/bedrockloader/mixin/ResourcePackManagerMixin.java
@@ -0,0 +1,33 @@
+package net.easecation.bedrockloader.mixin;
+
+import com.google.common.collect.ImmutableSet;
+import net.easecation.bedrockloader.BedrockLoader;
+import net.easecation.bedrockloader.resourcepack.BedrockLoaderResourcePackProvider;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.ResourcePackProvider;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
+
+@Mixin(ResourcePackManager.class)
+public class ResourcePackManagerMixin {
+	@Mutable
+	@Final
+	@Shadow
+	private Set<ResourcePackProvider> providers;
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	public void register(CallbackInfo ci) {
+		BedrockLoader.INSTANCE.getLogger().info("ResourcePackManagerMixin init");
+		this.providers = ImmutableSet.<ResourcePackProvider>builder()
+				.addAll(this.providers)
+				.add(new BedrockLoaderResourcePackProvider())
+				.build();
+	}
+}
