70130f4446130e6ef41526bbee4a91c662b1b7c6
yuxuanchiadm <yuxuanchiadm@126.com>
2025-04-16 09:24:18 +0800

    Update bedrock blockstate property NBT codec

--- src/main/kotlin/net/easecation/bedrockloader/block/property/BedrockBooleanProperty.kt
+++ src/main/kotlin/net/easecation/bedrockloader/block/property/BedrockBooleanProperty.kt
@@ -1,20 +1,35 @@
 package net.easecation.bedrockloader.block.property
 
+import com.mojang.serialization.Codec
+import com.mojang.serialization.DataResult
 import net.minecraft.state.property.Property
-import net.minecraft.util.StringIdentifiable
 import java.util.*
 
 data class BedrockBooleanProperty(
     private val bedrockName: String,
     private val name: String,
-    private val values: Set<Boolean>
+    private val values: Set<Boolean>,
+    private val specializedTag: Boolean
 ) : Property<Boolean>(bedrockName, Boolean::class.javaObjectType), BedrockProperty<Boolean, BedrockBooleanProperty> {
     companion object {
-        fun of(bedrockName: String, values: Set<Boolean>): BedrockBooleanProperty {
+        fun of(bedrockName: String, values: Set<Boolean>, specializedTag: Boolean = true): BedrockBooleanProperty {
             val name = bedrockName
-            return BedrockBooleanProperty(bedrockName, name, values)
+            return BedrockBooleanProperty(bedrockName, name, values, specializedTag)
         }
     }
+
+    private val codec: Codec<Boolean> = if (!specializedTag) super.getCodec() else Codec.BOOL.comapFlatMap(
+        {
+            when {
+                values.contains(it) -> DataResult.success(it)
+                else -> DataResult.error{ "Unable to read property: $this with value: $it" }
+            }
+        },
+        { it }
+    )
+    override fun getCodec(): Codec<Boolean> = codec
+    private val valueCodec: Codec<Value<Boolean>> = if (!specializedTag) super.getValueCodec() else this.codec.xmap(this::createValue, Value<Boolean>::value)
+    override fun getValueCodec(): Codec<Value<Boolean>> = valueCodec
 
     override val javaProperty get() = this
 
--- src/main/kotlin/net/easecation/bedrockloader/block/property/BedrockIntProperty.kt
+++ src/main/kotlin/net/easecation/bedrockloader/block/property/BedrockIntProperty.kt
@@ -1,19 +1,36 @@
 package net.easecation.bedrockloader.block.property
 
+import com.mojang.serialization.Codec
+import com.mojang.serialization.DataResult
 import net.minecraft.state.property.Property
 import java.util.*
+import java.util.function.Supplier
 
 data class BedrockIntProperty(
     private val bedrockName: String,
     private val name: String,
-    private val values: Set<Int>
+    private val values: Set<Int>,
+    private val specializedTag: Boolean
 ) : Property<Int>(bedrockName, Int::class.javaObjectType), BedrockProperty<Int, BedrockIntProperty> {
     companion object {
-        fun of(bedrockName: String, values: Set<Int>): BedrockIntProperty {
+        fun of(bedrockName: String, values: Set<Int>, specializedTag: Boolean = true): BedrockIntProperty {
             val name = bedrockName
-            return BedrockIntProperty(bedrockName, name, values)
+            return BedrockIntProperty(bedrockName, name, values, specializedTag)
         }
     }
+
+    private val codec: Codec<Int> = if (!specializedTag) super.getCodec() else Codec.INT.comapFlatMap(
+        {
+            when {
+                values.contains(it) -> DataResult.success(it)
+                else -> DataResult.error{ "Unable to read property: $this with value: $it" }
+            }
+        },
+        { it }
+    )
+    override fun getCodec(): Codec<Int> = codec
+    private val valueCodec: Codec<Value<Int>> = if (!specializedTag) super.getValueCodec() else this.codec.xmap(this::createValue, Value<Int>::value)
+    override fun getValueCodec(): Codec<Value<Int>> = valueCodec
 
     override val javaProperty get() = this
 
